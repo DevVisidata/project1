6aac65e74d8453bf858251d2c3947a9412629e41
DevVisidata <116703017+DevVisidata@users.noreply.github.com>
2022-11-29 15:27:05 +0700

    Update injourney_api.py

--- Project Injourney/injourney_api.py
+++ Project Injourney/injourney_api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
- 
+
 app = Flask(__name__)
 
 #KONFIG KE DB
@@ -26,43 +26,36 @@ def index():
         #             print(row)
     
     if request.method == 'POST':
-        print(request.form)
-        id_cluster = request.form['id_cluster']
-        cluster = request.form['cluster']
-        id_sub_cluster = request.form['id_sub_cluster']
-        sub_cluster = request.form['sub_cluster']
-        channel = request.form['channel']
-        b_realitation = request.form['b_realitation']
-        b_domestik = request.form['b_domestik']    
-        periode = request.form['periode']
-        id_member = request.form['id_member']
-        member_name = request.form['member_name']
-        dectotal = request.form['dectotal']
-        id_sub_class_component = request.form['id_sub_class_component']
-        sub_class_component = request.form['sub_class_component']
-        measurement_type = request.form['measurement_type']
-        remark = request.form['remark']
-        dectotal_by_month = request.form['dectotal_by_month']
-        data = [(id_cluster,cluster,id_sub_cluster,sub_cluster,channel,b_realitation,b_domestik,periode,id_member,member_name,dectotal,id_sub_class_component,sub_class_component,measurement_type,remark,dectotal_by_month)]
-        stmt = 'INSERT INTO production VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16)'
+        print(request.json)
+        id_cluster = request.json['id_cluster']
+        cluster = request.json['cluster']
+        id_sub_cluster = request.json['id_sub_cluster']
+        sub_cluster = request.json['sub_cluster']
+        channel = request.json['channel']
+        b_realitation = request.json['b_realitation']
+        b_domestik = request.json['b_domestik']    
+        periode = request.json['periode']
+        id_member = request.json['id_member']
+        member_name = request.json['member_name']
+        dectotal = request.json['dectotal']
+        id_sub_class_component = request.json['id_sub_class_component']
+        sub_class_component = request.json['sub_class_component']
+        measurement_type = request.json['measurement_type']
+        remark = request.json['remark']
+        data = [(id_cluster,cluster,id_sub_cluster,sub_cluster,channel,b_realitation,b_domestik,periode,id_member,member_name,dectotal,id_sub_class_component,sub_class_component,measurement_type,remark)]
+        stmt = 'INSERT INTO production VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15)'
         with conn:
             # conn.autocommit(True)
             with conn.cursor() as cur:
                 cur.executemany(stmt, data)
         return jsonify(data = list(data),isError= False, message= "Success", statusCode= 200 ), 200
 
-@app.route('/production/<id>', methods = ['POST', 'GET','PUT','DELETE'])
-def get_byId(id):
-    if request.method == 'GET':
-        cursor = conn.cursor()
-        cursor.execute(''' SELECT * FROM production WHERE id_cluster=%s ''',(id,))
-        data = cursor.fetchone()
-        return jsonify(data = data,isError= False, message= "Success", statusCode= 200 ), 200
-    if request.method == 'DELETE':
-        cursor = conn.cursor()
-        data = cursor.execute('''DELETE FROM users WHERE id = (%s)''',(id))
-        conn.connection.commit()
-        cursor.close()
-        return jsonify( message= "Success", statusCode= 200, data =data)
+@app.route('/production/')
+def prodcution():
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM production')
+    data = cursor.fetchall()
+    return jsonify(data = list(data),isError= False, message= "Success", statusCode= 200 ), 200
+
  
 app.run(host='0.0.0.0')
