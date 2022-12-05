83baa09d8777af7a6220ce02ece0bfd0dc41941f
DevVisidata <116703017+DevVisidata@users.noreply.github.com>
2022-12-05 10:27:31 +0700

    Update injourney_api.py

--- Project Injourney/injourney_api.py
+++ Project Injourney/injourney_api.py
@@ -9,53 +9,50 @@ conn = s2.connect('svc-3479b32e-b09c-4933-a069-d151ce16a097-dml.gcp-jakarta-1.sv
 #API FLASK
 @app.route('/production', methods = ['POST', 'GET'])
 def index():
-    
-    if request.method == 'GET':
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM production')
-        data = cursor.fetchall()
-        return jsonify(data = list(data),isError= False, message= "Success", statusCode= 200 ), 200
-
-        # for row in cursor.fetchall():
-        #     print(row)
-        # with conn:
-        #     # conn.autocommit(True)
-        #     with conn.cursor() as cur:
-        #         cur.execute('SELECT * FROM production')
-        #         for row in cur.fetchall():
-        #             print(row)
-    
     if request.method == 'POST':
-        print(request.json)
-        id_cluster = request.json['id_cluster']
-        cluster = request.json['cluster']
-        id_sub_cluster = request.json['id_sub_cluster']
-        sub_cluster = request.json['sub_cluster']
-        channel = request.json['channel']
-        b_realitation = request.json['b_realitation']
-        b_domestik = request.json['b_domestik']    
-        periode = request.json['periode']
-        id_member = request.json['id_member']
-        member_name = request.json['member_name']
-        dectotal = request.json['dectotal']
-        id_sub_class_component = request.json['id_sub_class_component']
-        sub_class_component = request.json['sub_class_component']
-        measurement_type = request.json['measurement_type']
-        remark = request.json['remark']
-        data = [(id_cluster,cluster,id_sub_cluster,sub_cluster,channel,b_realitation,b_domestik,periode,id_member,member_name,dectotal,id_sub_class_component,sub_class_component,measurement_type,remark)]
-        stmt = 'INSERT INTO production VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15)'
-        with conn:
-            # conn.autocommit(True)
-            with conn.cursor() as cur:
-                cur.executemany(stmt, data)
-        return jsonify(data = list(data),isError= False, message= "Success", statusCode= 200 ), 200
+        try:
+            api_key = request.headers['api_key']
+            if api_key == 'secret':
+                print(request.json)
+                id_cluster = request.json['id_cluster']
+                cluster = request.json['cluster']
+                id_sub_cluster = request.json['id_sub_cluster']
+                sub_cluster = request.json['sub_cluster']
+                channel = request.json['channel']
+                b_realitation = request.json['b_realitation']
+                b_domestik = request.json['b_domestik']    
+                periode = request.json['periode']
+                id_member = request.json['id_member']
+                member_name = request.json['member_name']
+                dectotal = request.json['dectotal']
+                id_sub_class_component = request.json['id_sub_class_component']
+                sub_class_component = request.json['sub_class_component']
+                measurement_type = request.json['measurement_type']
+                remark = request.json['remark']
+                data = [(id_cluster,cluster,id_sub_cluster,sub_cluster,channel,b_realitation,b_domestik,periode,id_member,member_name,dectotal,id_sub_class_component,sub_class_component,measurement_type,remark)]
+                stmt = 'INSERT INTO production VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15)'
+                with conn:
+                    # conn.autocommit(True)
+                    with conn.cursor() as cur:
+                        cur.executemany(stmt, data)
+                return jsonify(data = list(data),isError= False, message= "Create", statusCode= 201 ), 201
+            else:
+                return "Your key is false. Dont forget enter the key!"
+        except:
+            return "Dont forget enter the key"
 
-@app.route('/production/')
-def prodcution():
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM production')
-    data = cursor.fetchall()
-    return jsonify(data = list(data),isError= False, message= "Success", statusCode= 200 ), 200
+    if request.method == 'GET':
+        try:
+            api_key = request.headers['api_key']
+            if api_key == 'secret':
+                cursor = conn.cursor()
+                cursor.execute('SELECT * FROM production')
+                data = cursor.fetchall()
+                return jsonify(data = list(data),isError= False, message= "Success", statusCode= 200 ), 200
+            else:
+                return "Your key is false. Dont forget enter the key!"
+        except:
+            return "Dont forget enter the key"
 
  
 app.run(host='0.0.0.0')
